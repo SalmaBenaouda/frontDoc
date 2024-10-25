f4176c61658eff489003e782567c4ac714324b4a
SalmaBenaouda <Salma.benaouda@etu.uae.ac.ma>
2024-10-25 16:28:29 +0100

    Interfaces

--- src/app/modules/candidat/bourse/bourse.component.ts
+++ src/app/modules/candidat/bourse/bourse.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { AuthService } from '../../../services/auth/auth.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-bourse',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './bourse.component.html',
-  styleUrl: './bourse.component.css'
+  styleUrl: './bourse.component.css',
+  encapsulation: ViewEncapsulation.None,
 })
 export class BourseComponent {
+  constructor(private authService: AuthService) {}
 
+  onLogout() {
+    this.authService.logout();
+  }
 }
